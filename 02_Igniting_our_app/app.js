ae6825520e953b919c30da75017b9d5cd06ae8dc
asad.mukhtar <asadazad99@gmail.com>
2023-09-09 00:26:03 +0500

    chap 2 pending changes

--- 02_Igniting_our_app/app.js
+++ 02_Igniting_our_app/app.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const ele = React.createElement("div",{id:"heading"},
 [React.createElement("div" ,{id:"parent"},[
-    React.createElement("div",{},"Inside div"),
+    React.createElement("div",{},"Inside div d"),
     React.createElement("div",{},"Inside div2")
 ]),React.createElement("div",{id:"parentsibling"},[
     React.createElement("div",{},"Inside div"),
