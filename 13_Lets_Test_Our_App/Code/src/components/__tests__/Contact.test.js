71bc5905c8328e74d22d8750f753be8500f46f48
asad.mukhtar <asadazad99@gmail.com>
2023-12-27 14:39:05 +0500

    Added coverage folder in .gitignore file.

--- 13_Lets_Test_Our_App/Code/src/components/__tests__/Contact.test.js
+++ 13_Lets_Test_Our_App/Code/src/components/__tests__/Contact.test.js
@@ -2,14 +2,16 @@ import {render, screen} from "@testing-library/react";
 import ContactUs from '../ContactUs';
 import "@testing-library/jest-dom";
 // run , query , assert
-test("Should load the contact us component",()=>{
-    render(<ContactUs/>);
-    const heading = screen.getByRole("heading");
-    expect(heading).toBeInTheDocument();
-});
-test("Should check the number of text boxes",()=>{
-    render(<ContactUs/>);
-    const input = screen.getAllByRole("textbox");
-    console.log(input);
-    expect(input.length).toBe(2);
+describe("Contact us test cases",()=>{
+    test("Should load the contact us component",()=>{
+        render(<ContactUs/>);
+        const heading = screen.getByRole("heading");
+        expect(heading).toBeInTheDocument();
+    });
+    it("Should check the number of text boxes",()=>{
+        render(<ContactUs/>);
+        const input = screen.getAllByRole("textbox");
+        console.log(input);
+        expect(input.length).toBe(2);
+    });
 });
