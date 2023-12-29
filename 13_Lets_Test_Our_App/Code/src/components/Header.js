cececd26f421eadb6c18058358d1d02aa1562b2e
asad.mukhtar <asadazad99@gmail.com>
2023-12-29 18:58:14 +0500

    improved header styling.

--- 13_Lets_Test_Our_App/Code/src/components/Header.js
+++ 13_Lets_Test_Our_App/Code/src/components/Header.js
@@ -6,42 +6,45 @@ const Header = () => {
   const [toggleLogout, setToggleLogout] = useState("Logout");
   const cartItems = useSelector((store) => store.cart.items);
   return (
-    <div className="flex justify-between items-center mx-40">
-      <div className="logo-container">
-        <img className="w-28" src={LOGO_URL}></img>
-      </div>
-      <div className="nav-items">
-        <ul className="flex px-2">
-          <li className="item px-5">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="item px-5">
-            <Link to="/about">About us</Link>
-          </li>
-          <li className="item px-5">
-            <Link to="/contactus">Contact us</Link>
-          </li>
-          <li className="item px-5" >
-            <Link data-testid="cartlink" to="/cart">
-              Cart({cartItems ? cartItems.length : 0})
-            </Link>
-          </li>
-          <li className="item px-5">
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li className="item px-5">
-            <button
-              onClick={() => {
-                //toggleLogout = 'Login';
-                toggleLogout === "Logout"
-                  ? setToggleLogout("Login")
-                  : setToggleLogout("Logout");
-              }}
-            >
-              {toggleLogout}
-            </button>
-          </li>
-        </ul>
+    <div className="sticky top-0 bg-white z-50">
+      <div className="flex justify-between items-center mx-32">
+        <div className="logo-container">
+          <img className="w-24" src={LOGO_URL}></img>
+        </div>
+        <div className="nav-items">
+          <ul className="flex px-2">
+            <li className="item px-5 font-semibold uppercase text-xs hover:underline hover:transition-all">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="item px-5 font-semibold uppercase text-xs hover:underline hover:transition-all">
+              <Link to="/about">About us</Link>
+            </li>
+            <li className="item px-5 font-semibold uppercase text-xs  hover:underline hover:transition-all">
+              <Link to="/contactus">Contact us</Link>
+            </li>
+            <li className="item px-5 font-semibold uppercase text-xs  hover:underline hover:transition-all">
+              <Link data-testid="cartlink" to="/cart">
+                Cart({cartItems ? cartItems.length : 0})
+              </Link>
+            </li>
+            <li className="item px-5 font-semibold uppercase text-xs  hover:underline hover:transition-all">
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li className="item px-5 font-semibold uppercase text-xs  hover:underline hover:transition-all">
+              <button
+                className="uppercase text-xs font-semibold"
+                onClick={() => {
+                  //toggleLogout = 'Login';
+                  toggleLogout === "Logout"
+                    ? setToggleLogout("Login")
+                    : setToggleLogout("Logout");
+                }}
+              >
+                {toggleLogout}
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
